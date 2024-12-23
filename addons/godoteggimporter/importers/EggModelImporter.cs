060131523a8b4d467deeefe75e9b9dce4a57f32b
Autumn Rivers <natirivers@protonmail.ch>
2024-12-23 16:54:52 -0500

    Adding in bones work Although, they don't really do anything for now

--- addons/godoteggimporter/importers/EggModelImporter.cs
+++ addons/godoteggimporter/importers/EggModelImporter.cs
@@ -12,7 +12,7 @@ public partial class EggModelImporter : EditorImportPlugin
 {
     public override string _GetImporterName()
     {
-        return "autumnrivers.panda3d.egg";
+        return "autumnrivers.panda3d.egg_model";
     }
 
     public override string _GetVisibleName()
@@ -64,11 +64,6 @@ public partial class EggModelImporter : EditorImportPlugin
     {
         return new Array<Dictionary>()
         {
-            new Dictionary()
-            {
-                { "name", "force_animation" },
-                { "default_value", false }
-            },
             new Dictionary()
             {
                 { "name", "automatically_convert_collisions" },
@@ -134,6 +129,40 @@ public partial class EggModelImporter : EditorImportPlugin
                     ParseGroupData(subgroup);
                 }
             }
+            if(entityGroup.Members.Any(m => m is Joint))
+            {
+                var joints = entityGroup.Members.Where(m => m is Joint);
+                //var joints = entityGroup.Members.OfType<Joint>().ToList();
+                var skeleton = root.GetNodeOrNull("Skeleton3D");
+                if(skeleton == null)
+                {
+                    Skeleton3D skel = new Skeleton3D();
+                    skel.Name = "Skeleton3D";
+                    skeleton = skel;
+                }
+                var skeleton3d = (Skeleton3D)skeleton;
+
+                var skin = new Skin();
+                foreach (var joint in joints)
+                {
+                    var pJoint = (Joint)joint;
+                    AddBonesToSkeleton(pJoint, ref skeleton3d, ref skin);
+                    //skin = skeleton3d.CreateSkinFromRestTransforms();
+                    skeleton3d.RegisterSkin(skin);
+                }
+
+                root.AddChild(skeleton3d);
+                skeleton3d.Owner = root;
+
+                var mesh = root.GetNodeOrNull<MeshInstance3D>(group.Name);
+                if(mesh != null)
+                {
+                    mesh.Skin = skin;
+                    mesh.Reparent(skeleton3d);
+                    mesh.Skeleton = mesh.GetPathTo(skeleton3d);
+                    mesh.Owner = root;
+                }
+            }
         }
 
         scene.Pack(root);
@@ -150,6 +179,7 @@ public partial class EggModelImporter : EditorImportPlugin
         return new Node3D();
     }
 
+    // TODO: Rewrite this with SurfaceTool
     private MeshInstance3D ParsePolygonGroup(EntityGroup group, Panda3DEgg egg, Dictionary options = null)
     {
         MeshInstance3D meshInstance = new MeshInstance3D();
@@ -173,7 +203,10 @@ public partial class EggModelImporter : EditorImportPlugin
                 { "texture_filepath", string.Empty },
                 { "uvs", new Array<Vector2>() },
                 { "vertices", new Array<Vector3>() },
-                { "colors", new Array<Color>() }
+                { "colors", new Array<Color>() },
+                { "bones", new Array<int>() },
+                { "bone_weights", new Array<float>() },
+                { "indices", new Array<int>() }
             }
         };
 
@@ -190,7 +223,6 @@ public partial class EggModelImporter : EditorImportPlugin
                 {
                     string filename = filepath.Split("maps/")[1];
                     filepath = (string)options["change_maps_directory"] + '/' + filename;
-                    GD.Print(filepath);
                 }
                 AddTextureFilepathToSurface(surfaceName, filepath);
                 if(FileAccess.FileExists(filepath))
@@ -203,9 +235,19 @@ public partial class EggModelImporter : EditorImportPlugin
             int vertIndex = 0;
             foreach (var vert in polygon.VertexRef.Indices)
             {
+                AddIndexToSurface(surfaceName, vert);
+                bool hasAssociatedJoints = egg.Joints.Any(j => j.VertexRef.Any(v => v.Indices.Contains(vert)));
+                if(hasAssociatedJoints)
+                {
+                    var associatedJoints = egg.Joints.Where(j => j.VertexRef.Any(v => v.Indices.Contains(vert)));
+                    foreach (var associatedJoint in associatedJoints)
+                    {
+                        AddBoneToSurface(surfaceName, egg.Joints.IndexOf(associatedJoint));
+                    }
+                }
                 Vector3 vertex3;
                 Vertex referencedVertex = GetVertexFromPool(egg, vert, polygon.VertexRef.Pool);
-                // We swap X and Z because Panda3D is a Z-Up game engine
+                // We swap Y and Z because Panda3D is a Z-Up game engine
                 vertex3 = new Vector3(referencedVertex.X, referencedVertex.Z, referencedVertex.Y);
                 verticies[vertIndex] = vertex3;
                 vertIndex++;
@@ -231,6 +273,7 @@ public partial class EggModelImporter : EditorImportPlugin
             }
             AddVerticesToSurface(surfaceName, verticies);
         }
+
         foreach(var surface in surfaces)
         {
             // STUPID FIX ALERT // STUPID FIX ALERT //
@@ -242,6 +285,7 @@ public partial class EggModelImporter : EditorImportPlugin
             surfaceArray[(int)Mesh.ArrayType.Vertex] = ((Array<Vector3>)surface["vertices"]).ToArray();
             surfaceArray[(int)Mesh.ArrayType.TexUV] = ((Array<Vector2>)surface["uvs"]).ToArray();
             surfaceArray[(int)Mesh.ArrayType.Color] = ((Array<Color>)surface["colors"]).ToArray();
+            //surfaceArray[(int)Mesh.ArrayType.Index] = ((Array<int>)surface["indices"]).ToArray();
             polygonMesh.AddSurfaceFromArrays(Mesh.PrimitiveType.Triangles, surfaceArray);
         }
         for (int i = 0; i < polygonMesh.GetSurfaceCount(); i++)
@@ -263,7 +307,10 @@ public partial class EggModelImporter : EditorImportPlugin
                 { "texture_filepath", string.Empty },
                 { "uvs", new Array<Vector2>() },
                 { "vertices", new Array<Vector3>() },
-                { "colors", new Array<Color>() }
+                { "colors", new Array<Color>() },
+                { "bones", new Array<int>() },
+                { "bone_weights", new Array<float>() },
+                { "indices", new Array<int>() }
             });
             StandardMaterial3D mat = new();
             if ((bool)options["set_materials_to_unshaded"])
@@ -284,15 +331,6 @@ public partial class EggModelImporter : EditorImportPlugin
             surfaces[index]["vertices"] = verts;
         }
 
-        void AddUVsToSurface(string surfaceName, Array<Vector2> uvs)
-        {
-            var surface = surfaces.FirstOrDefault(s => (string)s["name"] == surfaceName);
-            if (surface == default) return;
-            int index = surfaces.IndexOf(surface);
-            //surfaces[index]["uvs"] = uvs;
-            ((Array<Vector2>)surfaces[index]["uvs"]).AddRange(uvs);
-        }
-
         void AddUVToSurface(string surfaceName, Vector2 uv)
         {
             var surface = surfaces.FirstOrDefault(s => (string)s["name"] == surfaceName);
@@ -309,15 +347,6 @@ public partial class EggModelImporter : EditorImportPlugin
             int index = surfaces.IndexOf(surface);
             //surfaces[index]["colors"] = colors;
             ((Array<Color>)surfaces[index]["colors"]).Add(color);
-        }
-
-        void AddColorsToSurface(string surfaceName, Array<Color> colors)
-        {
-            var surface = surfaces.FirstOrDefault(s => (string)s["name"] == surfaceName);
-            if (surface == default) return;
-            int index = surfaces.IndexOf(surface);
-            //surfaces[index]["colors"] = colors;
-            ((Array<Color>)surfaces[index]["colors"]).AddRange(colors);
         }
 
         bool CheckIfSurfaceExists(string surfaceName)
@@ -346,6 +375,22 @@ public partial class EggModelImporter : EditorImportPlugin
         {
             polygonMats[idx].AlbedoTexture = tex;
         }
+
+        void AddIndexToSurface(string surfaceName, int index)
+        {
+            var surface = surfaces.FirstOrDefault(s => (string)s["name"] == surfaceName);
+            if (surface == default) return;
+            int surfaceIndex = surfaces.IndexOf(surface);
+            ((Array<int>)surfaces[surfaceIndex]["indices"]).Add(index);
+        }
+
+        void AddBoneToSurface(string surfaceName, int boneIndex)
+        {
+            var surface = surfaces.FirstOrDefault(s => (string)s["name"] == surfaceName);
+            if (surface == default) return;
+            int surfaceIndex = surfaces.IndexOf(surface);
+            ((Array<int>)surfaces[surfaceIndex]["bones"]).Add(boneIndex);
+        }
     }
 
     private CollisionShape3D ParseCollisionGroup(EntityGroup group)
@@ -357,16 +402,71 @@ public partial class EggModelImporter : EditorImportPlugin
         return collisionShape;
     }
 
+    private void AddBonesToSkeleton(Joint jointGroup, ref Skeleton3D skeleton, ref Skin skin, int parentIndex = -1)
+    {
+        int boneIndex = skeleton.AddBone(jointGroup.Name);
+        Transform3D currentTransform;
+        if(jointGroup.Transform == null)
+        {
+            currentTransform = Transform3D.Identity;
+            if(boneIndex == 0) currentTransform = currentTransform.Rotated(new Vector3(1, 0, 0), Mathf.DegToRad(-90));
+        } else
+        {
+            currentTransform = GetTransformFromMatrix4(jointGroup.Transform.Matrix4);
+        }
+        Transform3D defaultTransform;
+        if(jointGroup.DefaultPose == null)
+        {
+            defaultTransform = currentTransform;
+        } else
+        {
+            defaultTransform = GetTransformFromMatrix4(jointGroup.DefaultPose.Matrix4);
+        }
+        skeleton.SetBonePose(boneIndex, currentTransform);
+        skeleton.SetBoneRest(boneIndex, defaultTransform);
+        skin.AddBind(boneIndex, currentTransform);
+        skin.SetBindName(boneIndex, jointGroup.Name);
+        if (parentIndex > -1) skeleton.SetBoneParent(boneIndex, parentIndex);
+
+        foreach (var joint in jointGroup.Joints)
+        {
+            AddBonesToSkeleton(joint, ref skeleton, ref skin, boneIndex);
+        }
+
+        Transform3D GetTransformFromMatrix4(double[,] matrix4)
+        {
+            Vector3 basis_col1 = new Vector3((float)matrix4[0, 0], (float)matrix4[0, 1], (float)matrix4[0, 2]);
+            Vector3 basis_col2 = new Vector3((float)matrix4[1, 0], (float)matrix4[1, 1], (float)matrix4[1, 2]);
+            Vector3 basis_col3 = new Vector3((float)matrix4[2, 0], (float)matrix4[2, 1], (float)matrix4[2, 2]);
+            Vector3 origin = new Vector3((float)matrix4[3, 0], (float)matrix4[3, 1], (float)matrix4[3, 2]);
+            Basis matrixBasis = new Basis(basis_col1, basis_col2, basis_col3);
+            var transform = new Transform3D(matrixBasis, origin);
+
+            // In a million years...
+            // In multiple universes...
+            // Even with all of the world's knowledge...
+            // I could NOT tell you why this fixes a major bug.
+            // I. DO. NOT. UNDERSTAND. WHY.
+            if(boneIndex == 0)
+            {
+                GD.Print(jointGroup.Name);
+                transform = transform.Rotated(new Vector3(1, 0, 0), Mathf.DegToRad(-90));
+            }
+
+            return transform;
+        }
+    }
+
     private Vertex GetVertexFromPool(Panda3DEgg egg, int index, string poolName)
     {
-        var pool = egg.Data.FirstOrDefault(g => (g is VertexPool) && g.Name == poolName);
+        var pool = egg.VertexPools.FirstOrDefault(p => p.Name == poolName);
 
         if(pool == default)
         {
             throw new Exception("Invalid vertex pool " + poolName);
         }
 
-        var poolRef = (VertexPool)pool;
+        var poolRef = pool;
         return poolRef.References.FirstOrDefault(v => v.Index == index);
     }
 }
